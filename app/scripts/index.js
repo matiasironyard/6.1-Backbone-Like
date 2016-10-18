caa0dfcb55e0833b96236f22222b694be63ffff7
Matias Mariani <matiasironyard@gmail.com>
2016-10-18 11:01:21 -0400

    tune up

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -2,11 +2,12 @@ var Like = require('./models/like').Like;
 var $ = require('jquery');
 var _ = require('underscore');
 
-var newLike = new Like({'like': 0});
+var newLike = new Like();
 console.log(newLike);
 
-$('.like').on('click', function(){
-  // event.preventDefault();
+$('.like').on('click', function(event){
+  event.preventDefault();
+  // 11 calls the method we created.
   newLike.setCounter();
   $('.likes-counter').html(newLike.get('like'));
   // console.log(newLike);
--- app/scripts/models/like.js
+++ app/scripts/models/like.js
@@ -7,30 +7,31 @@ var Like = Backbone.Model.extend({
     'like': 0,
     'hello': 'hello',
   },
-
-  initialize: function(){
-  },
-
+  // initialize: function(){
+  // }, NOT NEEDED
   setCounter: function() {
-    var newLike = this.get('like') + 1;
-    this.set('like', newLike);
-    console.log(newLike);
-    return newLike;
+    this.set('like', this.get('like') + 1);
+    // TOP code is better. dry.
+    // var newLike = this.get('like') + 1;
+    // this.set('like', newLike);
+    // console.log(newLike);
+    // return this;
   },
 
-
-  toJSON: function(){
+  toJSON: function(options){
     //toJSON must be called in the index.
     if(this.get('like') === 1){
-      $('.pluralize').html(' Like');
+      $('.pluralize').html(' Like'); }
+    else if (this.get('like') >=10){
+      $('.pluralize').html(' Like!!!');
     } else {
       $('.pluralize').html(' Likes');
     }
-    // this is needed to reset toJSON to do what it is supposed to do if we call it again in the code.
-    // we are returning it to its original state.
     return _.clone(this.attributes);
   }
 });
+//  32 this is needed to reset toJSON to do what it is supposed to do if we call it again in the code.
+// we are returning it to its original state.
 
 module.exports = {
   'Like' : Like,
